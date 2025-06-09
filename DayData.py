86f359d5c656963bbfb7de56e239d5f0df761c1c
Vincent <vincent930618@gmail.com>
2025-06-09 22:22:51 +0800

    chart

---
+++ DayData.py
@@ -0,0 +1,24 @@
+import requests
+import pandas as pd
+import datetime as dt
+from tabulate import tabulate
+
+''' 取得個股日成交資訊 '''
+
+# 輸入股票代號
+stock_id = '2330'
+# 當日時間
+date = dt.date.today().strftime("%Y%m%d")
+
+# 取得證交所網站資料
+stock_data = requests.get(
+    f'https://www.twse.com.tw/rwd/zh/ \
+        afterTrading/STOCK_DAY?date={date}&stockNo={stock_id}',
+    verify=False)
+
+json_data = stock_data.json()
+df = pd.DataFrame(data=json_data['data'],
+                  columns=json_data['fields'])
+print(tabulate(df.tail(), headers='keys', tablefmt='pretty', showindex=False))
+print('hello')
+print('haha')
